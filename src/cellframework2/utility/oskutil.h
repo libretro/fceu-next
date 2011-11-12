a51c7c0f9180ce5ddeec825fbaf9ec5083616e6d
TwinAphex51224 <twinaphex1@gmail.com>
2011-11-12 07:44:58 +0100

    (PS3) 'Save Settings as Shader Preset' works now - can set filename and title - also fixed OSK util

--- src/cellframework2/utility/oskutil.h
+++ src/cellframework2/utility/oskutil.h
@@ -7,20 +7,22 @@
 #include <stdlib.h>
 #include <string.h>
 
+
 typedef struct
 {
-   unsigned int osk_memorycontainer;
-   wchar_t init_message[CELL_OSKDIALOG_STRING_SIZE + 1]; // buffer
-   wchar_t message[CELL_OSKDIALOG_STRING_SIZE + 1]; //buffer
-   wchar_t osk_text_buffer[CELL_OSKDIALOG_STRING_SIZE + 1];
-   char osk_text_buffer_char[CELL_OSKDIALOG_STRING_SIZE + 1];
-   uint32_t flags;
-   bool is_running;
-   sys_memory_container_t containerid;
-   CellOskDialogPoint pos;
-   CellOskDialogInputFieldInfo inputFieldInfo;
-   CellOskDialogCallbackReturnParam outputInfo;
-   CellOskDialogParam dialogParam;
+	unsigned int osk_memorycontainer;
+	wchar_t init_message[CELL_OSKDIALOG_STRING_SIZE + 1]; // buffer
+	wchar_t message[CELL_OSKDIALOG_STRING_SIZE + 1]; //buffer
+	wchar_t osk_text_buffer[CELL_OSKDIALOG_STRING_SIZE + 1];
+	char osk_text_buffer_char[CELL_OSKDIALOG_STRING_SIZE + 1];
+	uint32_t flags;
+	bool is_running;
+	bool text_can_be_fetched;
+	sys_memory_container_t containerid;
+	CellOskDialogPoint pos;
+	CellOskDialogInputFieldInfo inputFieldInfo;
+	CellOskDialogCallbackReturnParam outputInfo;
+	CellOskDialogParam dialogParam;
 } oskutil_params;
 
 #define OSK_IS_RUNNING(object) object.is_running
@@ -33,10 +35,11 @@ extern "C" {
 void oskutil_write_message(oskutil_params *params, const wchar_t* msg);
 void oskutil_write_initial_message(oskutil_params *params, const wchar_t* msg);
 void oskutil_init(oskutil_params *params, unsigned int containersize);
-bool oskutil_abort(oskutil_params *params);
 bool oskutil_start(oskutil_params *params);
 void oskutil_stop(oskutil_params *params);
+void oskutil_finished(oskutil_params *params);
 void oskutil_close(oskutil_params *params);
+void oskutil_unload(oskutil_params *params);
 
 #ifdef __cplusplus
 }
