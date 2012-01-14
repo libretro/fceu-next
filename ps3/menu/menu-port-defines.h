1ba7b2321b2f76d27c027f39e4d117f6f1d1026f
TwinAphex51224 <twinaphex1@gmail.com>
2012-01-14 17:13:54 +0100

    (PS3) Added missing menu-port-defines.h

---
+++ ps3/menu/menu-port-defines.h
@@ -0,0 +1,14 @@
+/******************************************************************************* 
+ * menu-port-defines.h - FCEU Next PS3
+ *
+ *  Created on: Jan 9, 2012
+********************************************************************************/
+
+#define ROM_EXTENSIONS "fds|FDS|zip|ZIP|nes|NES|unif|UNIF"
+
+#define EMU_MENU_TITLE "FCEU |"
+#define VIDEO_MENU_TITLE "FCEU VIDEO |"
+#define AUDIO_MENU_TITLE "FCEU AUDIO |"
+
+#define EXTRA_SELECT_FILE_PART1()
+#define EXTRA_SELECT_FILE_PART2()
