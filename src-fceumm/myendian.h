8f6bfdeebf161a3913821002b4fca2f254de2a20
Twinaphex <autechre1024@hotmail.com>
2012-04-11 20:25:12 +0200

    Updates for libretro

--- src-fceumm/myendian.h
+++ src-fceumm/myendian.h
@@ -7,7 +7,7 @@ int write16le(uint16 b, FILE *fp);
 int write32le(uint32 b, FILE *fp);
 
 #ifdef __LIBRETRO__
-#include "../libsnes-fceumm/memstream.h"
+#include "../libretro-fceumm/memstream.h"
 int write32le_mem(uint32 b, memstream_t *mem);
 int read32le_mem(uint32 *Bufo, memstream_t *mem);
 #endif
