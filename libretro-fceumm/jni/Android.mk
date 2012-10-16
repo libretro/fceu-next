72e3860138235d8123121363d2d9e5397011a808
twinaphex <autechre1024@hotmail.com>
2012-10-16 20:03:59 +0200

    (Android) Fixed build

--- libretro-fceumm/jni/Android.mk
+++ libretro-fceumm/jni/Android.mk
@@ -2,19 +2,12 @@ LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
 
-LOCAL_MODULE    := libretro
-FCEU_SRC_DIRS := ../src-fceumm ../src-fceumm/boards ../src-fceumm/input ../src-fceumm/mappers
+LOCAL_MODULE    := retro
+FCEU_SRC_DIRS := ../../src-fceumm ../../src-fceumm/boards ../../src-fceumm/input ../../src-fceumm/mappers
 
-FCEU_SRC_FILES := $(wildcard $(LOCAL_PATH)/../src-fceumm/*.c) \
-		$(wildcard $(LOCAL_PATH)/../src-fceumm/boards/*.c) \
-		$(wildcard $(LOCAL_PATH)/../src-fceumm/input/*.c) \
-		$(wildcard $(LOCAL_PATH)/../src-fceumm/mappers/*.c)
+FCEU_CSRCS := $(foreach dir,$(FCEU_SRC_DIRS),$(wildcard $(dir)/*.c))
 
-FCEU_SRC_FILES := $(FCEU_SRC_FILES:$(LOCAL_PATH)/%=%)
-
-LOCAL_SRC_FILES  = ../libretro-fceumm/libretro.c \
-		../libretro-fceumm/memstream.c \
-		$(FCEU_SRC_FILES)
+LOCAL_SRC_FILES  = ../libretro.c ../memstream.c $(FCEU_CSRCS)
 
 LOCAL_CFLAGS = -DINLINE=inline -DSOUND_QUALITY=0 -DPATH_MAX=1024 -DPSS_STYLE=1 -DLSB_FIRST -D__LIBRETRO__ -DHAVE_ASPRINTF
 
