bff959ab8bde7ba185dab1a728a847bada99005b
twinaphex <autechre1024@hotmail.com>
2012-10-19 04:46:23 +0200

    (Android) Now compiles libraries for all Android platforms - ARM, ARMv7a, MIPS, and x86

--- libretro-fceumm/jni/Android.mk
+++ libretro-fceumm/jni/Android.mk
@@ -2,6 +2,18 @@ LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
 
+ifeq ($(TARGET_ARCH),arm)
+LOCAL_CFLAGS += -DANDROID_ARM
+endif
+
+ifeq ($(TARGET_ARCH),x86)
+LOCAL_CFLAGS +=  -DANDROID_X86
+endif
+
+ifeq ($(TARGET_ARCH),mips)
+LOCAL_CFLAGS += -DANDROID_MIPS -D__mips__ -D__MIPSEL__
+endif
+
 LOCAL_MODULE    := retro
 FCEU_SRC_DIRS := ../../src-fceumm ../../src-fceumm/boards ../../src-fceumm/input ../../src-fceumm/mappers
 
@@ -9,6 +21,6 @@ FCEU_CSRCS := $(foreach dir,$(FCEU_SRC_DIRS),$(wildcard $(dir)/*.c))
 
 LOCAL_SRC_FILES  = ../libretro.c ../memstream.c $(FCEU_CSRCS)
 
-LOCAL_CFLAGS = -DINLINE=inline -DSOUND_QUALITY=0 -DPATH_MAX=1024 -DPSS_STYLE=1 -DLSB_FIRST -D__LIBRETRO__ -DHAVE_ASPRINTF
+LOCAL_CFLAGS += -DINLINE=inline -DSOUND_QUALITY=0 -DPATH_MAX=1024 -DPSS_STYLE=1 -DLSB_FIRST -D__LIBRETRO__ -DHAVE_ASPRINTF
 
 include $(BUILD_SHARED_LIBRARY)
